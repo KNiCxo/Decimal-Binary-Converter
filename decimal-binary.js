00d477747f84f2ccf3194e308c7a27aab463eac8
KNiC.xo <ntcaliwag@hotmail.com>
2023-08-11 03:07:23 -0700

    Now determines if input is a fraction and parses it into a 'whole number' and 'fraction' string

--- decimal-binary.js
+++ decimal-binary.js
@@ -10,9 +10,13 @@ let swapButton = document.querySelector('.js-swapButton');
 let resultElement = document.querySelector('.js-result');
 
 // Boolean to check if converting to binary or decimal
-let isBinary = true;
+let isDecimal = true;
 
-const checkValid = (value) => {
+let isFraction = false;
+
+let periodPos = 0;
+
+const checkInput = (value) => {
   let periodCount = 0;
   if (value.match('[^.0-9]')) {
     resultElement.innerHTML = `Error: Must only contain decimal digits`;
@@ -24,19 +28,43 @@ const checkValid = (value) => {
   for (let i = 0; i < Number(value.length); i++) {
     if ((/[.]/).test(value.charAt(i))) {
       periodCount++;
+      periodPos = i;
     }
   }
 
   if (periodCount > 1) {
     resultElement.innerHTML = `Error: Must only contain one decimal point`;
+  } else if (periodCount === 1) {
+    isFraction = true;
+  } else {
+    isFraction = false;
   }
+  console.log(`Is fraction ${isFraction}`);
+  console.log(`Position of decimal point ${periodPos}`);
 }
+
+const handleInput = (value) => {
+  let wholeNumber;
+  let fraction;
+  if (isFraction) {
+    if (periodPos === 0) {
+      wholeNumber = '';
+      fraction = value.substring(0, value.length);
+    } else {
+      wholeNumber = value.substring(0, periodPos);
+      fraction = '0.' + value.substring(periodPos + 1, value.length);
+    }
+  }
+  console.log(wholeNumber);
+  console.log(fraction);
+};
 
 const decimalToBinary = (decimalValue) => {
 
 };
 
 convertButton.addEventListener('click', () => {
-  checkValid(userInput.value);
+  checkInput(userInput.value);
+  handleInput(userInput.value);
 });
 
